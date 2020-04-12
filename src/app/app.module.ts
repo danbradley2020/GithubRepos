de59c1de26f6715c562cad372e1b84183b5888d6
candycrushpro <bradleynilmar6702@gmail.com>
2020-04-12 16:47:23 +0300

    add navbar.component and about.component to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
