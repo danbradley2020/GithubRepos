4fdf3ccb5313e08bd954a0250effccc15d3e86b7
candycrushpro <bradleynilmar6702@gmail.com>
2020-04-12 13:31:10 +0300

    add Github User and Github repositories

---
+++ src/app/github.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+// import {User} from './user';
+// import {Repos} from './repos';
+// import {environment} from '../environments/environment';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+  username: string;
+
+  constructor() { }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"dd7c00cace10e6be985239a4ee5fa78829e82e86"
 };
 
 /*
