06db7c9d525dfa407eae279e83b8652570819bc5
candycrushpro <bradleynilmar6702@gmail.com>
2020-04-13 06:58:11 +0300

    add githubservice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ProfileComponent} from './profile/profile.component';
+// import {ProfileComponent} from './profile/profile.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -8,7 +8,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent},
+  // { path: 'profile', component: ProfileComponent},
   { path: 'about', component: AboutComponent},
   { path:'**', component:NotFoundComponent},
   { path: '', redirectTo:"/goals", pathMatch:"full"},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +20,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule,
   ],
